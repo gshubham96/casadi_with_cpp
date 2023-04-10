f0aee2e913c4da30fd721091cd70cf7b28b25ee9
gshubham96 <gshubham96@gmail.com>
2023-04-10 15:40:34 +0200

    minor

--- autonaut/dynamics_gen.cpp
+++ autonaut/dynamics_gen.cpp
@@ -50,8 +50,8 @@ int main(){
     std::cout << "x_dot 1 = " << x_dot(arg_1) << std::endl;
 
     casadi::DMDict x_dot_out = x_dot(casadi::DMDict{{"sym_x", sym_x}, {"sym_u", sym_u}, {"sym_p", sym_p}});
+    std::cout << "x_dot 2 = " << x_dot_out << std::endl;
     casadi::SX sym_xn = x_dot_out.at("x_dot_out");
-    std::cout << "x_dot 2 = " << sym_xn << std::endl;
 
 
     return 0;
