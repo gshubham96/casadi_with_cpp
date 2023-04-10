86beed5f182f022597a497b0550894fd9c46e835
gshubham96 <gshubham96@gmail.com>
2023-04-10 14:18:12 +0200

    minor

--- autonaut/solver_gen.cpp
+++ autonaut/solver_gen.cpp
@@ -26,7 +26,7 @@ int main(){
     // input arguements
     std::vector<casadi::SX> arg = {X, sym_p};
 
-    std::cout << "function arg : " << fname_n_in(fun_obj) << std::endl;
+    std::cout << "function arg : " << casadi::fname_n_in(fun_obj) << std::endl;
 
 
     // // Objective
