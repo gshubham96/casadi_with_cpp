36921e772145cfd16cb627dd1c9c1cd19496eab0
gshubham96 <gshubham96@gmail.com>
2023-04-11 08:50:25 +0200

    minor

--- autonaut/test_sym.cpp
+++ autonaut/test_sym.cpp
@@ -1,21 +1,20 @@
 #include <casadi/casadi.hpp>
 
 int main() {
-    // Define variables
-    casadi::SX x = casadi::SX::sym("x", 2);
-    casadi::SX y = casadi::SX::sym("y", 1);
+  // Define symbolic variables
+  casadi::SX x = casadi::SX::sym("x", 2);
+  casadi::SX y = casadi::SX::sym("y", 1);
 
-    // Define function f
-    casadi::SX f = x(0) * x(1);
+  // Define function f = x[0]*x[1]
+  casadi::SX f = x[0]*x[1];
+  casadi::Function f_func("f", {x}, {f});
 
-    // Define function y
-    y = f;
+  // Define y = f(x)
+  casadi::SXDict args;
+  args["x"] = x;
+  casadi::SX f_eval = f_func(args);
+  y = f_eval;
+  std::cout << "y = " << y << std::endl;
 
-    // Create function
-    casadi::Function fun("fun", {x}, {y});
-
-    // Print function
-    std::cout << fun << std::endl;
-
-    return 0;
+  return 0;
 }
