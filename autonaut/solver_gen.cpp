7187e237e62e3792ad44c2aef40588650aa6acbc
gshubham96 <gshubham96@gmail.com>
2023-04-13 10:14:23 +0200

    minor

--- autonaut/solver_gen.cpp
+++ autonaut/solver_gen.cpp
@@ -180,7 +180,7 @@ class MpcProblem {
         // sym_dx(1) = ssa(sym_dx(1));
 
         for(int j = 0; j < nx; j++)
-            g.push_back(sym_dx(j));
+            g.push_back(sym_dx[j]);
 
         std::cout << "3 g = " << g << std::endl;
 
