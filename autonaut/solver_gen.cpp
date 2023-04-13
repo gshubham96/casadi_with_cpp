623560b16bac908592b58afa2dc27c5c0a62fb33
gshubham96 <gshubham96@gmail.com>
2023-04-13 10:18:05 +0200

    minor

--- autonaut/solver_gen.cpp
+++ autonaut/solver_gen.cpp
@@ -175,7 +175,7 @@ class MpcProblem {
         // set initial state
         for(int j = 0; j < nx; j++)
             sym_dx.push_back(X(j,1) - p_x0(j));
-        sym_dx(1) = ssa(sym_dx(1));
+        sym_dx[1] = ssa(sym_dx[1]);
 
         for(int j = 0; j < nx; j++)
             g.push_back(sym_dx[j]);
