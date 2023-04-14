ebc460429e48adea03154bab7ec02b60e6e3f178
gshubham96 <gshubham96@gmail.com>
2023-04-14 18:16:04 +0200

    minor

--- autonaut/solver_gen.cpp
+++ autonaut/solver_gen.cpp
@@ -282,7 +282,10 @@ class MpcProblem {
         res = solver(arg);
         // std::cout << "optimal input found that is: " << res.at("x") << std::endl;
         auto optimized_vars = res.at("x");
-        
+
+        arg["x0"]  = optimized_vars;
+        res = solver(arg);
+
         std::ofstream file;
         std::string filename = "test.m";
         file.open(filename.c_str());
