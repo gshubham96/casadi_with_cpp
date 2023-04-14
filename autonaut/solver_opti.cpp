828275f7a7a64eb172a587b9fdab367c404d4cbc
gshubham96 <gshubham96@gmail.com>
2023-04-14 11:53:33 +0200

    minor

--- autonaut/solver_opti.cpp
+++ autonaut/solver_opti.cpp
@@ -90,7 +90,8 @@ class MpcProblem {
 
         // constant parameters for test - Vc, beta_c, Vw, beta_w,
         chi_d = 0;
-        Vc = 0.35; beta_c = 1.57; Vw = 5; beta_w = 1.57; k_1 = 0.9551; k_2 = -0.031775;
+        // Vc = 0.35; beta_c = 1.57; Vw = 5; beta_w = 1.57; k_1 = 0.9551; k_2 = -0.031775;
+        Vc = 0; beta_c = 1.57; Vw = 0; beta_w = 1.57; k_1 = 0.9551; k_2 = -0.031775;
 
         // system params
         double D11, R11, INV_M11;
