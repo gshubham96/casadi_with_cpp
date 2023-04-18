e26699715075eef357e2a4697c8f71768cd77f1b
gshubham96 <gshubham96@gmail.com>
2023-04-18 09:09:26 +0200

    minor

--- autonaut/CourseController.cpp
+++ autonaut/CourseController.cpp
@@ -318,20 +318,20 @@ namespace NMPC{
                 solver = casadi::nlpsol("solver", "ipopt", nlp, opts);
                 solver.generate_dependencies("nlp.c");
 
-                // Just-in-time compilation?
-                bool jit = false;
-                if (jit) {
-                    // Create a new NLP solver instance using just-in-time compilation
-                    // casadi::Dict optsi = {"compiler": "shell", "jit": True, "jit_options": {"compiler": "gcc","flags": ["-O3"]}};
-                    solver = casadi::nlpsol("solver", "ipopt", "nlp.c");
-                } else {
-                    // Compile the c-code
-                    int flag = system("gcc -fPIC -shared -O3 nlp.c -o nlp.so");
-                    casadi_assert(flag==0, "Compilation failed");
+                // #TODO Just-in-time compilation?
+                // bool jit = false;
+                // if (jit) {
+                //     // Create a new NLP solver instance using just-in-time compilation
+                //     // casadi::Dict optsi = {"compiler": "shell", "jit": True, "jit_options": {"compiler": "gcc","flags": ["-O3"]}};
+                //     solver = casadi::nlpsol("solver", "ipopt", "nlp.c");
+                // } else {
+                //     // Compile the c-code
+                //     int flag = system("gcc -fPIC -shared -O3 nlp.c -o nlp.so");
+                //     casadi_assert(flag==0, "Compilation failed");
 
-                    // Create a new NLP solver instance from the compiled code
-                    solver = casadi::nlpsol("solver", "ipopt", "nlp.so");
-                }
+                //     // Create a new NLP solver instance from the compiled code
+                //     solver = casadi::nlpsol("solver", "ipopt", "nlp.so");
+                // }
 
                 std::cout << "checkpoint 6: " << std::endl;
                 // define state bounds
