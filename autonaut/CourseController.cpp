e7be815a3fa99c2c236897499cdc32e0019d9b06
gshubham96 <gshubham96@gmail.com>
2023-04-19 17:42:04 +0200

    minor

--- autonaut/CourseController.cpp
+++ autonaut/CourseController.cpp
@@ -730,7 +730,7 @@ int main(){
     // set default parameters
     std::map<std::string, double> params_d;
     params_d["Vc"] = 0.35; params_d["beta_c"] = 1.57;
-    params_d["Vw"] = 0; params_d["beta_w"] = 1.57;
+    params_d["Vw"] = 5; params_d["beta_w"] = 1.57;
     params_d["k_1"] = 0.9551; params_d["k_2"] = -0.031775;
     params_d["Q"] = 4.5; params_d["R"] = 1.5;
 
@@ -762,7 +762,7 @@ int main(){
     chi_ref = 0.5;
     nmpc.updateMpcReference(chi_ref);
     nmpc.updateMpcState(state_d);
-    
+
     if(nmpc.optimizeMpcProblem())
         std::cout << "optimization succesful" << std::endl;
     else
