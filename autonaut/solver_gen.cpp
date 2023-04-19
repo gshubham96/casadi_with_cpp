6d4911c99f0999b7d1d0e28b74d996448506c009
gshubham96 <gshubham96@gmail.com>
2023-04-19 11:51:37 +0200

    minor

--- autonaut/solver_gen.cpp
+++ autonaut/solver_gen.cpp
@@ -328,7 +328,7 @@ class MpcProblem {
 
         std::ofstream file;
         std::string filename = fs::current_path().parent_path().string();
-        filename = filename + "/results";
+        filename = filename + "/results/solver_gen/m";
         std::cout << "file path: " << filename << std::endl;
         // file.open(filename.c_str());
         // file << "% Results file from " __FILE__ << std::endl;
