888759e082527b56b2815c6bd894dd7855824ab2
gshubham96 <gshubham96@gmail.com>
2023-04-19 12:37:48 +0200

    minor

--- autonaut/solver_gen.cpp
+++ autonaut/solver_gen.cpp
@@ -87,8 +87,8 @@ class MpcProblem {
         nx = 4; nu = 1; np = 11;
 
         // constant parameters for test - Vc, beta_c, Vw, beta_w,
-        chi_d = 0.2;
-        Vc = 0.35; beta_c = 1.57; Vw = 0; beta_w = 1.57; k_1 = 0.9551; k_2 = -0.031775;
+        chi_d = 0.35;
+        Vc = 0.35; beta_c = 1.57; Vw = 5; beta_w = 1.57; k_1 = 0.9551; k_2 = -0.031775;
 
         // system params
         double D11, R11, INV_M11;
