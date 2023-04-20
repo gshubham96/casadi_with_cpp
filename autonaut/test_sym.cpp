c4783d7ab0c6ef1717c85e87ff28c62dc055206c
gshubham96 <gshubham96@gmail.com>
2023-04-20 16:55:08 +0200

    minor

--- autonaut/test_sym.cpp
+++ autonaut/test_sym.cpp
@@ -1,27 +1,38 @@
-#include <casadi/casadi.hpp>
+#include <CourseController.h>
 
-int main() {
-  // Define symbolic variables
-  casadi::SX x = casadi::SX::sym("x", 2);
-  casadi::SX y = casadi::SX::sym("y", 1);
+int main(){
 
-  // Define function f = x[0]*x[1]
-  casadi::SX f = x(0)*x(1);
-  casadi::Function f_func("f", {x}, {f});
+    // instantiate a controller with default values
+    NMPC::CourseController nmpc;
 
-  // Define y = f(x)
-  casadi::SXDict args;
-  args["i0"] = x;
-  casadi::SXDict f_eval = f_func(args);
+    // set default parameters
+    std::map<std::string, double> params_d;
+    params_d["Vc"] = 0.35;      params_d["beta_c"] = 1.57;
+    params_d["Vw"] = 5;         params_d["beta_w"] = 1.57;
+    params_d["k_1"] = 0.9551;   params_d["k_2"] = -0.031775;
+    params_d["Q"] = 4.5;        params_d["R"] = 1.5;
+    nmpc.updateMpcParams(params_d);
 
-  // print contents of Dict
-  for (auto& item : f_eval) {
-    std::cout << "Key: " << item.first << std::endl;
-    std::cout << "Value: " << item.second << std::endl;
-  }
+    // update MPC state
+    std::map<std::string, double> state_d;
+    state_d["psi"] = 0.091855;
+    state_d["u"] = 0.9821;
+    state_d["v"] = 0.19964;
+    state_d["r"] = 0.031876;
+    nmpc.updateMpcState(state_d);
 
-  y = f_eval["o0"];
-  std::cout << "y = " << y << std::endl;
+    // update MPC reference
+    double chi = 0.53;
+    nmpc.updateMpcReference(chi);
 
-  return 0;
+    // solve the optimization problem
+    if(!nmpc.optimizeMpcProblem())
+        std::cerr << "optimization FAILED :(" << std::endl;
+    else
+        std::cout << "Optimal input is: " << nmpc.getOptimalInput() << std::endl;
+        
+    nmpc.saveTrajectoryToFile();
+    nmpc.print_details();
+
+    return 0;
 }
